31fab716a2db7b252246bfd45d56dec5cdd5d7b1
Hector Peraza <hperaza65@gmail.com>
2022-08-05 18:22:28 +0200

    Added 'maze' example, illustrates how to link T3XZ code with external assembly modules.

---
+++ progdev/t3xz/test/mkmaze.cmd
@@ -0,0 +1,3 @@
+mac =rsxio/i$/s8
+t3x maze
+tkb maze,maze,maze=maze,rsxio,lb:[system]t3xz/lb/q/of:t/task=...maz/ext=4096
---
+++ progdev/t3xz/test/mkmaze.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -x
+zxcc zsm4 =rsxio
+zxcc ../t3x maze
+zxcc tkb -"maze,maze,maze=maze.obj,rsxio,t3xz/lb/q/of:t/task=...maz/ext=4096"
